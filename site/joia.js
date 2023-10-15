51fdb145cad47a7fa51896af7d340009741bf732
gi <135059916+giovannang07@users.noreply.github.com>
2023-10-15 16:48:17 -0300

    arquivos do site

---
+++ site/joia.js
@@ -0,0 +1,98 @@
+let joiaJson = [
+    {
+        id: 1,
+        name: 'Colar Poseidon',
+        img: 'images/colar1.png',
+        price: [980.00, 986.00, 989.00],
+        sizes: [
+          'Pequeno',
+          'Médio',
+          'Grande'
+        ],
+        description: 'Um colar majestoso, adornado com uma safira azul profunda, refletindo o poder dos mares de Poseidon'
+      },
+      {
+        id: 2,
+        name: 'Brinco Afrodite',
+        img: 'images/brinco.png',
+        price: [280.00, 350.00, 380.00],
+        sizes: [
+          'Pequeno',
+          'Médio',
+          'Grande'
+        ],
+        description: 'Um brinco exuberante, ornado com uma pérola radiante, evocando a beleza e o encanto de Afrodite.'
+      },
+      {
+        id: 3,
+        name: 'Anel Atena',
+        img: 'images/anel.png',
+        price: [480.00, 560.00, 580.00],
+        sizes: [
+          'Pequeno',
+          'Médio',
+          'Grande'
+        ],
+        description: 'Um anel de Atena, com um design elegante, simbolizando a sabedoria e a destreza intelectual.'
+      },
+      {
+        id: 4,
+        name: 'Pingente Percy Jackson',
+        img: 'images/pingente.png',
+        price: [750.00, 888.00, 989.00],
+        sizes: [
+          'Pequeno',
+          'Médio',
+          'Grande'
+        ],
+        description: 'Um pingente de Percy Jackson, que representa seu domínio sobre o mar como um semideus filho de Poseidon.'
+      },
+      {
+        id: 5,
+        name: 'Pulseira Annabeth',
+        img: 'images/pulseira.png',
+        price: [980.00, 970.00, 990.00],
+        sizes: [
+          'Pequeno',
+          'Médio',
+          'Grande'
+        ],
+        description: 'Uma pulseira de Annabeth, com contas em tons de bronze e prata, simbolizando sua inteligência e determinação como uma semideusa filha de Atena.'
+      },
+      {
+        id: 6,
+        name: 'Colar Atena',
+        img: 'images/colar2.png',
+        price: [150.00, 178.00, 196.90],
+        sizes: [
+          'Pequeno',
+          'Médio',
+          'Grande'
+        ],
+        description: 'Um colar de Atena representando sabedoria e estratégia'
+      },
+      {
+        id: 7,
+        name: 'Anel atena',
+        img: 'images/pandora.png',
+        price: [900.00, 910.00, 920.00],
+        sizes: [
+          'Pequeno',
+          'Médio',
+          'Grande'
+        ],
+        description: 'Mussarela, banana, canela e açúcar'
+      },
+      {
+        id: 8,
+        name: 'Anel Zeus',
+        img: 'images/anel3.png',
+        price: [800.00, 820.00, 850.00],
+        sizes: [
+          'Pequeno',
+          'Médio',
+          'Grande'
+        ],
+        description: 'Um anel de Zeus, com cristais azuis reluzentes, simbolizando o poder dos céus e a majestade do rei dos deuses'
+      }
+];
